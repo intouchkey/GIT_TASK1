1e1a652ac1c2dfebe7b3174b367d215a6db51ea8
Thanatad Songpetchmongkol <thanatad@gmail.com>
2020-02-27 18:21:49 +0700

    add taxi and train class

--- question3.py
+++ question3.py
@@ -33,6 +33,22 @@ class Walk(Transportation):
       return cost
 
 # main program
+class Taxi(Transportation):
+    def __init__(self, start_place = "", end_place = "", distance = 0):
+        super().__init__(start_place, end_place, distance)
+    def find_cost(self):
+        cost = super().get_distance() * 40
+##        return ("From " + super().get_start_place() + " to " + super().get_end_place() + ", it will cost " + str(cost) + " Baht by taking a taxi")
+        return cost
+
+class Train(Transportation):
+    def __init__(self, start_place = "", end_place = "", distance = 0, stations = 0):
+        super().__init__(start_place, end_place, distance)
+        self.stations = stations
+    def find_cost(self):
+        cost = self.stations * 5
+##        return ("From " + super().get_start_place() + " to " + super().get_end_place() + ", it will cost " + str(cost) + " Baht by taking a train")
+        return cost
 
 travel_cost = 0
 
